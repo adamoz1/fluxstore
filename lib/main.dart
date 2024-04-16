be611d956ea9f78773a56a4e4b2be967278b415f
adamoz1 <jupiterleague8@gmail.com>
2024-04-16 10:26:14 +0530

    Added Library

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluxstore/screens/splash_screen1.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return GetMaterialApp(
+      home: SplashScreen1(),
     );
   }
 }
---
+++ lib/screens/splash_screen1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/splash_image1.png"),
+                    fit: BoxFit.cover)),
+          )
+        ],
+      ),
+    );
+  }
+}
