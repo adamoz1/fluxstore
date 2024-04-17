cf90f1e2c854351b8c4bc67d89d709f7a6efb5b7
GemStore <panchalchirag0210@gmail.com>
2024-04-17 09:46:34 +0530

    new updates

--- lib/Routes/app_routes.dart
+++ lib/Routes/app_routes.dart
@@ -4,8 +4,15 @@ import 'package:fluxstore/screens/notification_page.dart';
 import 'package:fluxstore/screens/splash_screen1.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
+import '../screens/profile_page.dart';
+
 appRoutes() => [
       GetPage(name: '/', page: () => const HomePage()),
       GetPage(name: '/notification', page:() => const NotificationPage()),
+      GetPage(name: '/profile',page: () => const ProfilePage()),
+      GetPage(name: '/splash1', page: () => const SplashScreen1()),
+      GetPage(name: '/splash2', page: () => SplashScreen2()),
+      GetPage(name: '/signin', page: () => Signin()),
+      GetPage(name: '/signup', page: () => Signup()),
 
     ];
---
+++ lib/common/bottom_navigation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items:
+
+    );
+  }
+}
